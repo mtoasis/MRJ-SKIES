a099ab03dbfdb8e4a2069e17155686013a0bd0d8
mtoasis <mtoasis22@gmail.com>
2019-11-16 13:25:54 -0800

    basic structure with homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        SKIES
+                    </h1>
+                    <span className="subtitle">
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        SNOWBOARDS
+                    </h1>
+                    <span className="subtitle">
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        CLOTHING
+                    </h1>
+                    <span className="subtitle">
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        ACCESSORIES
+                    </h1>
+                    <span className="subtitle">
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        SALE
+                    </h1>
+                    <span className="subtitle">
+                        SHOP NOW
+                    </span>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+)
+
+export default HomePage
