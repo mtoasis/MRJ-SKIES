5ff45fa230d305ea8284578db5b3fd8564427dba
mtoasis <mtoasis22@gmail.com>
2019-11-16 15:07:25 -0800

    menu-item field was updated

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {sections} from './directory.data'
+import MenuItem from '../menu-item/menu-item.component'
+import './directory.styles.scss'
+
+export default class Directory extends React.Component{
+
+    constructor(){
+        super();
+
+        this.state = {
+            sections: sections
+        }
+    }
+    
+    render(){
+        return(
+            <div className="directory-menu">
+                {
+                    this.state.sections.map(({title, imageUrl, id, size})=> (
+                        <MenuItem key ={id} title={title} imageUrl={imageUrl} size={size}/>
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+
---
+++ src/components/directory/directory.data.js
@@ -0,0 +1,34 @@
+export const sections = [
+  {
+    title: 'SKIS',
+    imageUrl: 'https://images.unsplash.com/photo-1551698618-1dfe5d97d256?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    id: 1,
+    linkUrl: 'shop/skies'
+  },
+  {
+    title: 'SNOWBOARDS',
+    imageUrl: 'https://images.unsplash.com/photo-1522056615691-da7b8106c665?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80',
+    id: 2,
+    linkUrl: 'shop/snowboards'
+  },
+  {
+    title: 'CLOTHINGS',
+    imageUrl: 'https://images.unsplash.com/photo-1453694595360-51e193e121fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1266&q=80',
+    id: 3,
+    linkUrl: 'shop/clothings'
+  },
+  {
+    title: 'ACCESSORIES',
+    imageUrl: 'https://images.unsplash.com/photo-1485839240317-550557b58b49?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    size: 'large',
+    id: 4,
+    linkUrl: 'shop/accessories'
+  },
+  {
+    title: 'SALE',
+    imageUrl: 'https://images.unsplash.com/photo-1526178613552-2b45c6c302f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    size: 'large',
+    id: 5,
+    linkUrl: 'shop/sale'
+  }
+];
