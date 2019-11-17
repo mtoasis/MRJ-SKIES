fb13b020a28c579e7c5d18fa8689af8a5543ec26
mtoasis <mtoasis22@gmail.com>
2019-11-17 00:54:27 -0800

    added cartIcon and cart-dropdown component. Cart reducer was added too

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
@@ -8,36 +8,33 @@ import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
+import {connect} from 'react-redux'
+
+import {setCurrentUser} from './redux/user/user.action'
+
+
+
 class App extends React.Component {
 
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
-
-  unsubscribeFromAuth = null
+    unsubscribeFromAuth = null
 
   componentDidMount() {
+    const {setCurrentUser} = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
+          setCurrentUser({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data()
             }
-          }, ()=>{
-            console.log(this.state)
           })
-        })
-        
+        })        
       }
-      this.setState({ currentUser: userAuth })
+      setCurrentUser(userAuth)
     })
   }
 
@@ -48,11 +45,14 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/signin' component={SignInAndSignUpPage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/signin' 
+          render={()=> this.props.currentUser? 
+          (<Redirect to='/'/>)
+          :(<SignInAndSignUpPage />)} />
         </Switch>
 
       </div>
@@ -60,4 +60,12 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapStateToProps = ({user})=>({
+  currentUser: user.currentUser
+})
+
+const mapDispatchToProps = (dispatch) =>({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utils'
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 import './header.styles.scss'
 
-const Header = ({ currentUser }) => (
+import { connect } from 'react-redux'
+
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -20,13 +24,25 @@ const Header = ({ currentUser }) => (
             {currentUser ? (
                 <div className='option' onClick={() => auth.signOut()}>
                     SIGN OUT
-          </div>
+                    </div>
             ) : (
                     <Link className='option' to='/signin'>
                         SIGN IN
-          </Link>
+                    </Link>
                 )}
+            <CartIcon />
         </div>
+        {hidden ?
+            null
+            : <CartDropdown />
+        }
+
     </div>
 );
-export default Header
+
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
+})
+
+export default connect(mapStateToProps)(Header);
